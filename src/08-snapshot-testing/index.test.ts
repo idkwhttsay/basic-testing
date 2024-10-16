fed4eaea249d81ceee38113f09c0c20d7d31c2ad
idkwhttsay <danil.tatarinov.00@gmail.com>
2024-10-16 19:07:24 -0400

    feat: snapshot-testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,28 @@
 // Uncomment the code below and write your tests
 // import { generateLinkedList } from './index';
 
+import { generateLinkedList } from './index';
+
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const input = [1];
+    const result = generateLinkedList(input);
+
+    expect(result).toStrictEqual({
+      next: {
+        next: null,
+        value: null,
+      },
+      value: 1,
+    });
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    const result = generateLinkedList(input);
+    expect(result).toMatchSnapshot();
   });
 });
